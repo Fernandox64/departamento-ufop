80975de2c0e8b9d1696ac1ca7c67c71866a24711
Fernando <fernandox64@gmail.com>
2026-09-04 10:24:02 -0300

    Ajusta botoes do admin as cores do tema

--- resources/views/partials/theme-vars.blade.php
+++ resources/views/partials/theme-vars.blade.php
@@ -6,9 +6,12 @@
         --brand-highlight: {{ $coresTema[0] }};
         --brand-gray-light: {{ $coresTema[1] }};
         --brand-wine: {{ $coresTema[2] }};
+        --brand-wine-rgb: {{ sscanf($coresTema[2], '#%02x%02x%02x')[0] }}, {{ sscanf($coresTema[2], '#%02x%02x%02x')[1] }}, {{ sscanf($coresTema[2], '#%02x%02x%02x')[2] }};
         --brand-red-strong: {{ $coresTema[3] }};
+        --brand-red-strong-rgb: {{ sscanf($coresTema[3], '#%02x%02x%02x')[0] }}, {{ sscanf($coresTema[3], '#%02x%02x%02x')[1] }}, {{ sscanf($coresTema[3], '#%02x%02x%02x')[2] }};
         --brand-surface: {{ $coresTema[4] }};
         --brand-charcoal: {{ $coresTema[5] }};
+        --brand-charcoal-rgb: {{ sscanf($coresTema[5], '#%02x%02x%02x')[0] }}, {{ sscanf($coresTema[5], '#%02x%02x%02x')[1] }}, {{ sscanf($coresTema[5], '#%02x%02x%02x')[2] }};
         --brand-gray: {{ $coresTema[1] }};
         --bs-primary: {{ $coresTema[3] }};
         --bs-primary-rgb: {{ sscanf($coresTema[3], '#%02x%02x%02x')[0] }}, {{ sscanf($coresTema[3], '#%02x%02x%02x')[1] }}, {{ sscanf($coresTema[3], '#%02x%02x%02x')[2] }};
